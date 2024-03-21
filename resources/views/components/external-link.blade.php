63bf2efb772fc11108a2f7d9aa4036daf725dbef
Paolo Dávila <paolo.davilab@gmail.com>
2024-03-21 14:53:12 +0100

    added url env

--- resources/views/components/external-link.blade.php
+++ resources/views/components/external-link.blade.php
@@ -4,43 +4,43 @@
             <h3 class="card-title">{{ __('Applications') }}</h3>
         </div>
         <div class="card-body">
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('INVENTARY_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-barcode"></i> {{ __('Inventory') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('PHONE_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-phone"></i> {{ __('Phones') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('ORACLE_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-th"></i> {{ __('Oracle Tablespaces') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('CHECKMK_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-check"></i> {{ __('Check MK') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('VCENTER_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-unlock"></i> {{ __('vCenter') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('VCENTER_MGMT_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-lock"></i> {{ __('vCenter MGMT') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('RACKTABLES_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-table"></i> {{ __('Racktables') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('SCMI_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-thermometer-empty"></i> {{ __('SCMI CPD-3C') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('NETAPP_3C_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-database"></i> {{ __('NETAPP 3C') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('NETAPP_SH_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-database"></i> {{ __('NETAPP SH') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('HPE_IRS_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-server"></i> {{ __('HPE IRS') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('SIMDCAT_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-cloud"></i> {{ __('SIMDCAT') }}
             </a>
-            <a href="#" class="btn btn-app" target="_blank">
+            <a href="{{ env('PACS_URL') }}" class="btn btn-app" target="_blank">
                 <i class="fas fa-paper-plane"></i> {{ __('PACS') }}
             </a>
         </div>
