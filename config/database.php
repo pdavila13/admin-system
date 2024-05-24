5bc017fb7a7093e3e6029d7c6165eeb5397eb1fc
Paolo Dávila <paolo.davilab@gmail.com>
2024-05-24 14:44:29 +0200

    Added second connection in databases

--- config/database.php
+++ config/database.php
@@ -63,6 +63,24 @@ return [
             ]) : [],
         ],
 
+        'inventory' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('INVENTORY_DB_DATABASE', 'forge'),
+            'username' => env('INVENTORY_DB_USERNAME', 'forge'),
+            'password' => env('INVENTORY_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
