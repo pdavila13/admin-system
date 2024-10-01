0e5eab1765873d2dc35209c5fbea4fcec74023ca
Paolo Dávila <paolo.davilab@gmail.com>
2024-10-01 14:29:46 +0200

    Added last_reboot column in vcenter_vms management

--- app/Http/Controllers/VCenterController.php
+++ app/Http/Controllers/VCenterController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\vCenterVM;
 use Illuminate\Http\Request;
 use App\Services\vCenterService;
-use Illuminate\Support\Facades\Http;
-use GuzzleHttp\Exception\RequestException;
 use Yajra\DataTables\Facades\DataTables;
 
 class VCenterController extends Controller
@@ -29,8 +27,7 @@ class VCenterController extends Controller
     public function index()
     {
         try {
-            //$this->vcenterService->fetchAndStoreVMs(); // Fetch and store VMs data
-            $vms = vCenterVM::all(); // Retrieve data from the database
+            $vms = vCenterVM::all();
             
             return view('admin.vcenter_vms.index', compact('vms'));
         } catch (\Exception $e) {
@@ -55,36 +52,9 @@ class VCenterController extends Controller
             'updated_vm' => $vm
         ]);
     }
-    // {
-    //     $vm = vCenterVM::findOrFail($request->id);
-    //     $column = $request->column;
-    //     $value = $request->value;
-
-    //     if (in_array($column, ['id', 'name', 'power_state', 'guest_OS', 'hardware_version', 'tools_version_status', 'upgrade_status'])) {
-    //         $vm->$column = $value;
-    //         $vm->save();
-
-    //         return response()->json(['success' => true]);
-    //     }
-
-    //     return response()->json(['success' => false], 400);
-    //     // $vmId = $request->input('id');
-    //     // $column = $request->input('column');
-    //     // $value = $request->input('value');
-
-    //     // $vm = vCenterVM::find($vmId);
-    //     // if ($vm) {
-    //     //     $vm->$column = $value;
-    //     //     $vm->save();
-
-    //     //     return response()->json(['success' => true]);
-    //     // }
-
-    //     // return response()->json(['success' => false], 404);
-    // }
 
     public function vms() {
-        $vms = vCenterVM::select('id','name','description','annotation','guest_OS','tools_version_status','hardware_version','upgrade_status')
+        $vms = vCenterVM::select('id','name','description','annotation','guest_OS','hardware_version','upgrade_status','last_reboot')
         ->where(function ($query) {
             $query->whereNull('upgrade_status')
                 ->orWhere('upgrade_status', '!=', 'NO');
